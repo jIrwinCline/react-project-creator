d5c37e9e74a2c0d686dbdfb727ff0984863df716
Jacob Irwin and Nick Ramsey <jacob@>
2019-07-18 15:34:38 -0700

    add pivitol git scripts to file creator

--- lib/project_creator.rb
+++ lib/project_creator.rb
@@ -5,8 +5,8 @@ require 'fileutils'
 
 p "Enter your project name:"
 project = gets.chomp
-FileUtils.mkdir("../#{project}")
-FileUtils.cd("../#{project}")
+FileUtils.mkdir("./#{project}")
+FileUtils.cd("./#{project}")
 FileUtils.mkdir("lib")
 p "What do you want to name your file?"
 file_name = gets.chomp
@@ -19,7 +19,8 @@ File.open("Gemfile", 'w') { |file| file.write("source 'https://rubygems.org'
 
 gem 'pry'
 gem 'rspec'
-gem 'fileutils'") }
+gem 'fileutils'
+gem 'pivotal_git_scripts'") }
 
 File.open("lib/#{file_name}.rb", 'w') { |file| file.write("require 'pry'
 require 'fileutils'
